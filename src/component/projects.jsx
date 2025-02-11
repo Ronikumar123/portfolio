299e94300871a62751d900fbeb5c8b6747c44ea9
roni <ronykumar85251@gmail.com>
2025-02-12 01:32:09 +0600

    update projects section

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -6,26 +6,37 @@ const projects = () => {
     <>
       <div className="container projects my-3"></div>
       <h1>Projects</h1>
-       <div className="row">
-  {project.map((data) => (
-    <>
-    <div key={data.id}>
-    <div className="card" style={{width:'18rem'}}>
-  <img src={data.imageSrc} className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-   </div>
-  </div>
-    </div>
-    </>
+       <div className="row d-flex justify-content-center align-content-center">
+        {project.map((data) => (
+          <>
+             <div key={data.id}className='my-4 col-sm-6 col-md-4 col-lg-3 mx-5'>
+              <div className="card bg-dark text-light" style={{width:'18rem', border:"1px solid yellow", boxShadow:"5px 5px 10px 10px rgba(101,175,10,0.5)"}}>
+                <div className="img p-3 d-flex justify-content-center align-content-center">
+                   <img src={data.imageSrc} className="card-img-top" alt="..."
+                     style={{
+                      width: "250px",
+                      height: "200px",
+                      border:'2px solid yellow',
+                      borderRadius:"10px"
+                  }}
+               />
+        
+      </div>
+               <div className="card-body text-center">
+                  <h5 className="card-title">{data.title}</h5>
+                   <p className="card-text">{data.description}</p>
+                    <a href={data.demo} className="btn btn-primary mx-3">Demo</a>
+                    <a href={data.source} className="btn btn-warning">Code</a>
+                  </div>
+                </div>
+            </div>
+           </>
 
-  ))}
-       </div>
+          ))}
+         </div>
        
-    </>
-  )
-}
+       </>
+      )
+     }
 
 export default projects
